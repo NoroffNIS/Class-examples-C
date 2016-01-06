df7ff8874435462913205cd920784fbbe2949f7b
Brage Roalkvam <brage89@gmail.com>
2016-01-06 09:34:43 +0100

    more variabels example

--- Variabels/main.c
+++ Variabels/main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#define PI 3.14159265359
 
 int main()
 {
-    int one = 1;
-    double two = 2.2;
-    double sum = one * two;
-    printf("Equals %f \n", sum);
-    return 0;
 
 }
 
@@ -22,4 +20,19 @@ int main()
     scanf(" %s", &input_number2);
     printf("Equals %s - %d \n", input_number1, sizeof(input_number1));
     printf("Equals %s - %d ", input_number2, sizeof(input_number2));
+
+    Triangle calculation of hypotenuse:
+    a^2 = b^2 + c^2 2bc cos(alpha)
+    a = sqrt(pow(b,2) + pow(c,2) - 2*b*c * cos(alpha * PI /180.0))
+
+    double a, b, c, alpha;
+    b=3;
+    c=4;
+    alpha = 90;
+
+    a = sqrt(pow(b,2) + pow(c,2) - 2*b*c * cos(alpha * PI /180.0));
+    printf("%f",a);
+
+
+
 */
