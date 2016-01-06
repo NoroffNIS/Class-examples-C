b52cdc2606953f1ad2582eb20e0af0bebf309118
Brage Roalkvam <brage89@gmail.com>
2016-01-06 08:36:43 +0100

    variabels example

---
+++ Variabels/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int one = 1;
+    double two = 2.2;
+    double sum = one * two;
+    printf("Equals %f \n", sum);
+    return 0;
+
+}
+
+/*
+    int one = 1;
+    double two = 2.2;
+    double sum = one * two;
+    printf("Equals %f \n", sum);
+
+    char input_number1[2], input_number2[2];
+    scanf(" %s", &input_number1);
+    scanf(" %s", &input_number2);
+    printf("Equals %s - %d \n", input_number1, sizeof(input_number1));
+    printf("Equals %s - %d ", input_number2, sizeof(input_number2));
+*/
