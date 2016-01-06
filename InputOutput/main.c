0a66d02c382592ca9e162a15c55cb86234a53401
Brage Roalkvam <brage89@gmail.com>
2016-01-06 11:35:34 +0100

    example updated

--- InputOutput/main.c
+++ InputOutput/main.c
@@ -3,25 +3,49 @@
 
 int main()
 {
-    /*Declaring the variables that is going to be used */
+    /*Declaring the variables that is going to be used. */
+    char first_name[20],last_name[20], street_name[20], state[20], email[20];
+    int phone_nr, street_number, zip_code;
+
+
+    /*Printing out text to user, and scanning the number typed. */
+    printf("Type your first name:\n");
+    scanf(" %s", &first_name);
+
+    printf("Type your last name:\n");
+    scanf(" %s", &last_name);
+
+    printf("Type your street name:\n");
+    scanf(" %s", &street_name);
+
+    printf("Type your street number:\n");
+    scanf(" %d", &street_number);
+
+    /*Printing out information typed in to user. */
+    printf("Your information is: \n");
+    printf("Name: %s %s \n", first_name, last_name);
+    printf("Address: %s %d \n", street_name, street_number);
+
+    return 0;
+}
+/*
     int input_number;
     char input_character;
     char input_text[50];
 
-    /*Printing out text to user, and scanning the number typed */
+    *Printing out text to user, and scanning the number typed *
     printf("Type in some number!\n");
     scanf("%d", &input_number);
     printf("You typed in: %d \n", input_number);
 
-    /*Printing out text to user, and scanning the character typed */
+    *Printing out text to user, and scanning the character typed *
     printf("Type in a character!\n");
     scanf(" %c", &input_character);
     printf("You typed in: %c \n", input_character);
 
-    /*Printing out text to user, and scanning the text typed */
+    *Printing out text to user, and scanning the text typed *
     printf("Type in some text!\n");
     scanf("%s", &input_text);
     printf("You typed in: %s \n", input_text);
 
-    return 0;
-}
+*/
--- Variabels/main.c
+++ Variabels/main.c
@@ -3,25 +3,46 @@
 #include <math.h>
 
 #define PI 3.14159265359
+/*265359*/
 
 int main()
 {
+    char first_array[4], second_array[4];
+    printf("Type some letters:\n");
+    scanf("%s", &first_array);
+
+    printf("Type some letters:\n");
+    scanf("%s", &second_array);
+
+    printf("This is what you typed: %s\n", first_array);
+    printf("This is what you typed: %s\n", second_array);
+
+   return 0;
 
 }
 
+
 /*
+    **int and double test**
     int one = 1;
     double two = 2.2;
     double sum = one * two;
     printf("Equals %f \n", sum);
 
+    **remainder**
+    int one = 7;
+    int two = 2;
+    double sum = one % two;
+    printf("Equals %f \n", sum);
+
+    **Char array memory overflow:**
     char input_number1[2], input_number2[2];
     scanf(" %s", &input_number1);
     scanf(" %s", &input_number2);
     printf("Equals %s - %d \n", input_number1, sizeof(input_number1));
     printf("Equals %s - %d ", input_number2, sizeof(input_number2));
 
-    Triangle calculation of hypotenuse:
+    **Triangle calculation of hypotenuse:**
     a^2 = b^2 + c^2 2bc cos(alpha)
     a = sqrt(pow(b,2) + pow(c,2) - 2*b*c * cos(alpha * PI /180.0))
 
